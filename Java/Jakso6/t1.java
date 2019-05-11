d773cbe22e348b4d3ddee6a8fa18c4494ee315bb
Jouni Kononen <45825600+Jounikononen@users.noreply.github.com>
2019-05-11 12:12:47 +0300

    Add files via upload

---
+++ Java/Jakso6/t1.java
@@ -0,0 +1,42 @@
+﻿/*
+ * Tekijä: Jouni Kononen
+ * Jakson numero: 6
+ * Jakson tehtävänumero: 1
+ * Päiväys: 9.5.2019
+ */
+
+import java.awt.*;
+import javax.swing.*;
+import java.net.*;
+import java.io.*;
+
+public class t1 {
+    public static void main(String[] args) throws Exception {
+        
+    //Luodaan käyttöliittymä
+    JFrame f= new JFrame("http://www.centria.fi");  
+    JTextArea t1;  
+    t1=new JTextArea("");  
+    t1.setBounds(50,100, 600,300);   
+    f.add(t1);  
+    f.setSize(700,700);  
+    f.setLayout(null);  
+    f.setVisible(true); 
+        
+        //Luetaan URLin tiedot
+        URL oracle = new URL("https://web.centria.fi/");
+        BufferedReader in = new BufferedReader(
+        new InputStreamReader(oracle.openStream()));
+        
+        //Käydään tietoja läpi rivi kerrallaan
+        String inputLine;
+        while ((inputLine = in.readLine()) != null)
+            
+            //Lisätään Tekstikenttään sivun tietoja rivi kerrallaan allekkain
+            t1.append(inputLine + "\n");
+        
+    }
+   
+    //Tee ScrollBarit ennen palautusta
+    
+}
