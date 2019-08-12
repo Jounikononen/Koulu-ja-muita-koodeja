cbbac886a7561a8c883142b97d8d8af12e628872
Jouni Kononen <45825600+Jounikononen@users.noreply.github.com>
2019-08-12 20:25:35 +0300

    Delete T_7.php

--- PHP/T_7.php
+++
@@ -1,8 +0,0 @@
-<?php
-$nimi = $_GET["nimi"];
-$viesti = $_GET["viesti"];
-
-if ($nimi == "" || $viesti == "")
-	 echo "Et antanut kaikkia tietoja!";
-	else echo $nimi . ": Heipä hei, mitä kuuluu?";
-?>
